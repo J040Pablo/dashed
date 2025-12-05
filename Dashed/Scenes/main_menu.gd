c387f37fbe26f272023e645bae86b1ca82eddc86
J040Pablo <aguadetorneira7@gmail.com>
2025-12-05 14:42:23 -0300

    Main Menu

---
+++ Dashed/Scenes/main_menu.gd
@@ -0,0 +1,51 @@
+extends Node2D
+
+var level: int = 1
+
+func _ready() -> void:
+	$CenterContainer/SettingsMenu/fullscreen.button_pressed = true if DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN else false  
+	$CenterContainer/SettingsMenu/mainvolslider.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master")))  
+	$CenterContainer/SettingsMenu/musicvolslider.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("MUSIC")))  
+	$CenterContainer/SettingsMenu/sfxvolslider.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("SFX")))   
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file(str("res://Dashed/Levels/", level, '.tscn'))
+	
+func _on_settings_pressed() -> void:
+	$CenterContainer/MainButtons.visible = false
+	$CenterContainer/SettingsMenu.visible = true
+	
+func _on_credits_pressed() -> void:
+	$CenterContainer/MainButtons.visible = false
+	$CenterContainer/SettingsMenu.visible = true
+	
+func _on_quit_pressed() -> void:
+	$CenterContainer/MainButtons.visible = true
+	$CenterContainer/SettingsMenu.visible = false
+	$CenterContainer/CreditsMenu.visible = false
+
+func _on_back_pressed() -> void:
+	$CenterContainer/MainButtons.visible = true
+	if $CenterContainer/SettingsMenu.visible:
+		$CenterContainer/SettingsMenu.visible = false
+		$CenterContainer/MainButtons/settigns.grab_focus()
+		
+	if $CenterContainer/CreditsMenu.visible:
+		$CenterContainer/CreditsMenu.visible = false
+		$CenterContainer/MainButtons/credits.grab_focus()
+
+func _on_fullscreen_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
+
+func _on_mainvolslider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Master"), value)
+
+
+func _on_musicvolslider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("MUSIC"), value)
+
+
+func _on_sfxvolslider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("SFX"), value)
